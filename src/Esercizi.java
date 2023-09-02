95288f8be4c3fe5a7d122e99fe33b85e7de36a5b
Enzo Ilazaj <enzo.ilazay@eng.it>
2023-09-02 21:27:53 +0200

    Esercizio n.1 concluso

--- src/Esercizi.java
+++ src/Esercizi.java
@@ -1,60 +1,54 @@
+import java.util.Arrays;
 
 public class Esercizi {
+
     public static void main(String[] args) {
-        int[] arrayToBalance = {12, -3, 22, 7};
-        boolean result = testBilanciamento(arrayToBalance);
-        if (result) {
-            System.out.println("The array is BALANCED.");
-        }
-        else {
-            System.out.println("The array is NOT balanced.");
-        }
+        int[] example1 = {-4, -4};
+        int[] example2 = {12, -3, 22, 7};
+        int[] example3 = {-3, 4, 100, -13, 9, -104, -8, -1};
+        int[] example4 = {24};
+        int[] example5 = {24, 1, 24};
+        int[] example6 = {0, 2, 2, 0};
+        int[] example7 = {};
+
+        runTestBilanciamento(example1);
+        runTestBilanciamento(example2);
+        runTestBilanciamento(example3);
+        runTestBilanciamento(example4);
+        runTestBilanciamento(example5);
+        runTestBilanciamento(example6);
+        runTestBilanciamento(example7);
     }
 
     private static boolean testBilanciamento(int[] r) {
-        if (hasNotRightSize(r.length)) return false;
-        int balanceSum = r[0] + r[r.length - 1];
-        int mirrorIndex = 0; //FORSE 1 ?
-        return exploreArrayRecursion(r, r[mirrorIndex], r[r.length - 1 - mirrorIndex], mirrorIndex, balanceSum);
+        if (hasNotRightSize(r.length)) return false; //If the size is NOT an even number OR the array is empty
+        int balanceSum = r[0] + r[r.length - 1]; //First sum is used as a reference for the balancing
+        int index = 0;
+        return exploreArrayRecursion(r, index, balanceSum); //Begin of recursion
     }
 
     private static boolean hasNotRightSize(int size) {
         return size % 2 != 0 || size == 0;
     }
 
-    private static boolean exploreArrayRecursion(int[] array, int leftSum, int rightSum, int mirrorIndex, int balanceSum) {
-         // CONTROLLA INDICE DESTRO-SINISTRO
-        if (array.length / 2 - 1 == mirrorIndex) return leftSum == rightSum;
-        mirrorIndex++;
-        return exploreArrayRecursion(array, leftSum + array[mirrorIndex], rightSum + array[array.length - 1 - mirrorIndex], mirrorIndex);
+    private static boolean exploreArrayRecursion(int[] r, int index, int balanceSum) {
+        int currentSum = r[index] + r[r.length - 1 - index];
+
+        if (currentSum != balanceSum) return false; //Current sum is not equal to the original balancing sum
+        if (r.length / 2 - 1 == index) return true; //End of recursion
+
+        index++;
+        return exploreArrayRecursion(r, index, balanceSum); //Recursion
     }
 
-//    // QUI AVEVO COMPRESO MALE L'ESERCIZIO MA MI DISPIACE LEVARLO :(
-//
-//    public static void main(String[] args) {
-//        int[] arrayToBalance = {12, -3, 22, 7};
-//        boolean result = testBilanciamento(arrayToBalance);
-//        if (result) {
-//            System.out.println("The array is BALANCED.");
-//        }
-//        else {
-//            System.out.println("The array is NOT balanced.");
-//        }
-//    }
-//
-//    private static boolean testBilanciamento(int[] r) {
-//        if (hasNotRightSize(r.length)) return false;
-//        int mirrorIndex = 0;
-//        return exploreArrayRecursion(r, r[mirrorIndex], r[r.length - 1 - mirrorIndex], mirrorIndex);
-//    }
-//
-//    private static boolean hasNotRightSize(int size) {
-//        return size % 2 != 0 || size == 0;
-//    }
-//
-//    private static boolean exploreArrayRecursion(int[] array, int leftSum, int rightSum, int mirrorIndex) {
-//        if (array.length / 2 - 1 == mirrorIndex) return leftSum == rightSum;
-//        mirrorIndex++;
-//        return exploreArrayRecursion(array, leftSum + array[mirrorIndex], rightSum + array[array.length - 1 - mirrorIndex], mirrorIndex);
-//    }
+    private static void runTestBilanciamento(int[] r) {
+        boolean result = testBilanciamento(r);
+        if (result) {
+            System.out.println("The array " + Arrays.toString(r) + " is: BALANCED.");
+        }
+        else {
+            System.out.println("The array " + Arrays.toString(r) + " is: NOT balanced.");
+        }
+    }
+
 }
